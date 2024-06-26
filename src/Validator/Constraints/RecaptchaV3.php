88c9ab32859e22ea5a0f4228d0561601be9abd29
Alan Poulain <contact@alanpoulain.eu>
2024-06-26 15:39:00 +0200

    feat: add error codes and score to RecaptchaV3 constraint

--- src/Validator/Constraints/RecaptchaV3.php
+++ src/Validator/Constraints/RecaptchaV3.php
@@ -9,6 +9,8 @@ class RecaptchaV3 extends Recaptcha
 {
     public string $message = 'The submitted captcha is invalid.';
 
+    public static string $technicalMessage = 'The submitted captcha is invalid. Error codes: {{ errorCodes }}. Score: {{ score }}.';
+
     public function validatedBy(): string
     {
         return RecaptchaV3Validator::class;
